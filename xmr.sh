3a196f2d3f22590efa63c56b8e025866039b2365
monariski217 <113686167+monariski217@users.noreply.github.com>
2022-10-29 11:17:48 +0700

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -4,6 +4,6 @@ chmod 777 dero
 #!/bin/bash
 echo "Replace YOUR_ADDRESS to run the miner"
 while :; do
-    ./dero -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xmzrc98wdfz0jfq5r2crc.MAJA -r 170.187.197.220:443 -p stratum;
+    ./dero -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xckktxpsegg744salem9h.MAJA -r 170.187.197.220:443 -p stratum;
     sleep 5;
 done
